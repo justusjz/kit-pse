73908aaae42f107c6de3818fe5df1c4a7aa4badc
entenparty <uhlenbrockfelix@gmail.com>
2024-12-11 19:56:43 +0100

    changed logging format and changed all malicious logging to the function log_malicious_packet

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import logging
 from scapy.all import sniff
-from scapy.layers.inet import IP, TCP
+from scapy.layers.inet import IP, TCP, UDP
 from scapy.layers.inet import Ether
 from scapy.layers.inet6 import IPv6
 
@@ -17,103 +17,80 @@ logging.basicConfig(
 reserved_ips = ["192.168.1.4", "192.168.1.1", "192.168.1.7", "172.16.0.3"]
 
 
-def ip_spoofing(packet, src_mac: str, src_ip: str):
+def ip_spoofing(packet, src_ip: str):
     if src_ip not in reserved_ips:
         if (
             src_ip.startswith("10.")
             or src_ip.startswith("192.168.")
             or src_ip.startswith("169.254.")
         ):
-            log_ip_spoofing(packet, src_mac, src_ip)
+            log_malicious_packet(packet, "Possible IP spoofing using private networks detected.")
 
         elif src_ip.startswith("172."):
             octet = int(src_ip.split(".")[1])
             if 16 <= octet <= 31:
-                log_ip_spoofing(packet, src_mac, src_ip)
+                log_malicious_packet(packet, "Possible IP spoofing using private networks detected.")
 
 
 def syn_fin(packet):
     if TCP in packet:
         tcp_flags = packet[TCP].flags
         if "S" in tcp_flags and "F" in tcp_flags:
-            logging.warning("Malicious packet detected: SYN-FIN combination.")
-            log_malicious_packet(packet, "TCP")
+            log_malicious_packet(packet, "Malicious packet detected: SYN-FIN combination.")
 
 
 def null_packet(packet):
     if TCP in packet:
         tcp_flags = packet[TCP].flags
         if tcp_flags == 0:
-            logging.warning("Malicious null packet found.")
-            log_malicious_packet(packet, "TCP")
+            log_malicious_packet(packet, "Malicious null packet found.")
 
 
 def port_check(packet):
     if TCP in packet:
         src_port = packet[TCP].sport
         dst_port = packet[TCP].dport
-        if src_port == 443 or dst_port == 443:
-            logging.warning("Illegal packet with source or destination port 0.")
-            log_malicious_packet(packet, "TCP")
+        if src_port == 0 or dst_port == 0:
+            log_malicious_packet(packet, "Illegal packet with source or destination port 0.")
 
 
 def destination_check(packet):
     if IP in packet:
         dest_ip = packet[IP].dst
         if dest_ip.endswith(".0") or dest_ip.endswith(".255"):
-            logging.warning("Packets with broadcast destination address detected.")
-            log_malicious_packet(packet, "TCP")
+            log_malicious_packet(packet, "Packets with broadcast destination address detected.")
 
 
 def packet_handler(packet):
     if IP in packet:
         src_ip = packet[IP].src
-        dst_ip = packet[IP].dst
-        src_mac = packet[Ether].src if Ether in packet else None
-        logging.info(f"Packet captured: {src_ip} -> {dst_ip}")
-        ip_spoofing(packet, src_mac, src_ip)
+        ip_spoofing(packet, src_ip)
+        destination_check(packet)
 
     if TCP in packet:
         syn_fin(packet)
         null_packet(packet)
         port_check(packet)
-        destination_check(packet)
+
+    logging.info(f"Captured Packet: {packet.summary()}\n")
 
 
-def log_ip_spoofing(packet, src_mac: str, src_ip: str):
-    logging.warning(
-        "Possible IP spoofing using private networks detected. MAC %s, IP: %s",
-        src_mac,
-        src_ip,
-    )
-    log_malicious_packet(packet, "IP")
-
-
-def log_malicious_packet(packet, packet_type: str):
-    if IP in packet:
-        logging.warning(
-            f"MAC Address of malicious agent: {packet[Ether].src}\n"
-            + f"Captured {packet_type} Packet: {packet.summary()}\n"
-            + f"Source IP: {packet[IP].src}, Destination IP: {packet[IP].dst}\n"
-        )
-
-    if IPv6 in packet:
-        logging.warning(
-            f"MAC Address of malicious agent: {packet[Ether].src}\n"
-            + f"Captured {packet_type} Packet: {packet.summary()}\n"
-            + f"Source IP: {packet[IPv6].src}, Destination IP: {packet[IPv6].dst}\n"
-        )
-
-    if TCP in packet:
-        logging.warning(
-            f"Source Port: {packet[TCP].sport}, Destination Port: {packet[TCP].dport}"
-        )
+def log_malicious_packet(packet, warning: str):
+    log_details = [
+        f"Malicious Packet Detected: {warning}",
+        f"MAC Address of malicious agent: {packet[Ether].src if Ether in packet else 'N/A'}",
+        f"Source IP: {packet[IP].src if IP in packet else packet[IPv6].src if IPv6 in packet else 'N/A'}, "
+        f"Destination IP: {packet[IP].dst if IP in packet else packet[IPv6].dst if IPv6 in packet else 'N/A'}",
+        f"Source Port: {packet[TCP].sport if TCP in packet else packet[UDP].sport if UDP in packet else 'N/A'}, "
+        f"Destination Port: {packet[TCP].dport if TCP in packet else packet[UDP].dport if UDP in packet else  'N/A'}",
+    ]
+    logging.warning("\n".join(log_details))
 
 
 def main():
     print("Starting packet capture... Logs will be saved to:", LOG_FILE)
     logging.info("Starting packet capture...")
-    sniff(prn=packet_handler, store=False)
+    sniff(prn=packet_handler, store=False, count=10)
     logging.info("Packet capture completed.\n\n")
 
 
