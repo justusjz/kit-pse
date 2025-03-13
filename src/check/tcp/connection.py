cf21f82eec330add6cb29282427c16e58bbaf6ea
Justus Zorn <84742176+justusjz@users.noreply.github.com>
2025-03-13 12:47:33 +0100

    Fix failing tests

--- src/check/tcp/connection.py
+++ src/check/tcp/connection.py
@@ -31,7 +31,7 @@ tcp_connections = dict[TcpConnection]()
 # in some tests, we're not performing a TCP handshake, but that
 # shouldn't matter, so we can ignore it with this method
 def tcp_handshake_ignore(src: str, dst: str, sport: int, dport: int):
-    conn = TcpConnection()
+    conn = TcpConnection(src, dport)
     conn.state = "ack"
     tcp_connections[(src, dst, sport, dport)] = conn
 
